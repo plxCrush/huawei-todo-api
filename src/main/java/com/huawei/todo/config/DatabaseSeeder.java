bb9236846c80e6a6e9666a4c2166659d89e0090f
plxCrush <e_gonen@hotmail.com>
2019-04-04 12:08:08 +0300

    Added database seeder.

---
+++ src/main/java/com/huawei/todo/config/DatabaseSeeder.java
@@ -0,0 +1,44 @@
+package com.huawei.todo.config;
+
+import com.huawei.todo.Application;
+import com.huawei.todo.models.User;
+import com.huawei.todo.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseSeeder implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public void run(String... strings) {
+
+        logger.info("Running database seeder...");
+
+        User emre = userService.get("emre");
+        if (emre == null) {
+            emre = new User();
+            emre.setName("Emre Gönen");
+            emre.setUsername("emre.gonen");
+            emre.hashPassword("123");
+            userService.save(emre);
+        }
+
+        User huawei = userService.get("huawei");
+        if (huawei == null) {
+            huawei = new User();
+            huawei.setName("Huawei User");
+            huawei.setUsername("huawei");
+            huawei.hashPassword("huawei");
+            userService.save(huawei);
+        }
+
+    }
+}
