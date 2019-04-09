5c8155adbbf69f5c880e6a281f4730db5ab6cefe
plxCrush <e_gonen@hotmail.com>
2019-04-09 16:15:24 +0300

    Review on api error messages.

--- src/main/java/com/huawei/todo/controllers/exceptions/ApiErrorMessage.java
+++ src/main/java/com/huawei/todo/controllers/exceptions/ApiErrorMessage.java
@@ -8,17 +8,15 @@ public enum ApiErrorMessage {
     UNEXPECTED_ERROR("An unexpected error occurred at server."),
 
     // Authentication Error Messages
-    PASSWORD_TOO_WEAK("Password too weak."),
     USERNAME_ALREADY_EXISTS("This username already exists."),
     INVALID_USERNAME_OR_PASSWORD("Invalid username or password."),
-    USERNAME_CANT_CONTAIN_FORBIDDEN_CHARACTERS("Username cannot contain forbidden characters."),
     PASSWORD_DOES_NOT_MATCH_REPEAT("Password does not match repeat"),
 
     // Misc...
     TODOLIST_NOT_FOUND("Todo list not found."),
     TODOITEM_NOT_FOUND("Todo item not found."),
     USER_NOT_FOUND("User not found."),
-    TODOITEM_HAVE_INCOMPLETED_DEPENDENCIES("Todo item have unmet dependencies."),
+    TODOITEM_HAVE_INCOMPLETED_DEPENDENCIES("Todo item have incomplete dependencies."),
     ITEMS_ARE_NOT_IN_THE_SAME_LIST("To-do items are not in the same list."),
     NOT_YOUR_TODOLIST("Not authorized");
 
